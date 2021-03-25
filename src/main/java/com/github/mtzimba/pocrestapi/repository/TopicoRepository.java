921e06c0a1ad18d6e32856845778bf6e370435f9
Matheus Cardoso <mtzimba@gmail.com>
2021-03-24 23:19:10 -0300

    spring-boot-seguranca-cache-monitoramento - implementação aula 1 - Paginação e Ordenação

--- src/main/java/com/github/mtzimba/pocrestapi/repository/TopicoRepository.java
+++ src/main/java/com/github/mtzimba/pocrestapi/repository/TopicoRepository.java
@@ -1,12 +1,12 @@
 package com.github.mtzimba.pocrestapi.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.mtzimba.pocrestapi.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	public List<Topico> findByCursoNome(String cursoNome);
+	public Page<Topico> findByCursoNome(String cursoNome, Pageable paginacao);
 }
